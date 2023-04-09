0375fe4cfb4e32fb80c9f496abfeb9272b27ecb9
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-09 17:01:04 +0500

    add notes endpoint with user authentications

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,41 @@
 const express=require('express');
 const router=express.Router();
+const Note=require('../models/Note');
+const fetchUser=require('../middleware/fetchUser');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    res.json([])
+
+router.get('/fetchallnotes',fetchUser,
+    async (req,res)=>{
+    const notes = await Note.find({user: req.user.id})
+    res.json([notes])
 })
 
+
+router.post('/addnote',fetchUser,
+    [
+    body('title').isLength({min:3}),
+    body('description').isLength({min:5}),
+    ],
+    async (req,res)=>{
+
+        try{
+
+            const {title,description,tag}=req.body;
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const note=new Note({
+                title,description,tag,user: req.user.id
+            })
+            const saveNote=await note.save()
+            res.json(saveNote)
+        }catch(error) {
+            console.log(error.message);
+            res.status(500).send("Internal Server error occured!")
+        }
+    })
 
 module.exports=router
