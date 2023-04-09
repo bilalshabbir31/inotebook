5bf76bcd27ff67b17554b3a5ffe60124d054f9c5
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-09 15:42:02 +0500

    add login route using jwt token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -36,14 +36,47 @@ router.post('/createuser',[body('email').isEmail(),body('name').isLength({ min: 
       }
 
       const authtoken= jwt.sign(data,JWT_SECERT);
-      console.log(authtoken);
-
       res.json({authtoken});
     }catch(error){
       console.log(error.message);
-      res.status(500).send("some error occured")
+      res.status(500).send("Internal Server error occured!")
     }
 })
 
+// authenticate a user
+
+router.post('/login',
+  [body('email').isEmail(),
+  body('password').exists(),],
+  async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+
+    const {email,password}=req.body;
+    try {
+      let user=await User.findOne({email});
+      if (!user){
+        return res.status(400).json({error:"Please try to login with correct credentials"});
+      }
+      const passwordcompare= await bcrypt.compare(password,user.password);
+      if (!passwordcompare){
+        return res.status(400).json({error:"Please try to login with correct credentials"});
+      }
+
+      const payload={
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken= jwt.sign(payload,JWT_SECERT);
+      res.send(authtoken);
+      } catch(error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server error occured!")
+    }
+  })
 
 module.exports=router
