6116caf9ce76fceb741c20892b25f6167d374561
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-09 14:42:49 +0500

    add salt in password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express= require('express');
 const User=require('../models/User');
 const router=express.Router();
 const { body, validationResult } = require('express-validator');
-
+const bcrypt=require('bcryptjs');
 
 // Create a user using: POST "api/auth/"
 router.post('/createuser',[body('email').isEmail(),body('name').isLength({ min: 5 }),body('password').isLength({ min: 5 })],
@@ -12,22 +12,25 @@ router.post('/createuser',[body('email').isEmail(),body('name').isLength({ min: 
       return res.status(400).json({ errors: errors.array() });
     }
 
-    let user = await User.findOne({ email: req.body.email });
-    if (user){
-      return res.status(400).json({error: "sorry user already exist"})
-    }
+    try{
 
-    user= await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
-    
-    res.json(user)
-    
-    // .then(user => res.json(user))
-    // .catch(err=>{console.log(err)
-    // res.json({error:"Please enter a unique value",message:err.message})})
+      let user = await User.findOne({ email: req.body.email });
+      if (user){
+        return res.status(400).json({error: "sorry user already exist"})
+      }
+      const salt=await bcrypt.genSalt(10);
+      const secPass=await bcrypt.hash(req.body.password,salt)
+      user= await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: secPass,
+      });
+      
+      res.json(user)
+    }catch(error){
+      console.log(error.message);
+      res.status(500).send("some error occured")
+    }
 })
 
 
