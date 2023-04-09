bfff62bcc6b05b83c4a4a21b45a9a414a6cc26ca
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-09 13:38:37 +0500

    change the port + add unique user validation+ make func aync

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,18 +5,29 @@ const { body, validationResult } = require('express-validator');
 
 
 // Create a user using: POST "api/auth/"
-router.post('/',[body('email').isEmail(),body('name').isLength({ min: 5 }),body('password').isLength({ min: 5 })],(req,res)=>{
+router.post('/createuser',[body('email').isEmail(),body('name').isLength({ min: 5 }),body('password').isLength({ min: 5 })],
+  async (req,res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user){
+      return res.status(400).json({error: "sorry user already exist"})
+    }
+
+    user= await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err=>{console.log(err)
-    res.json({error:"Please enter a unique value",message:err.message})})
+    });
+    
+    res.json(user)
+    
+    // .then(user => res.json(user))
+    // .catch(err=>{console.log(err)
+    // res.json({error:"Please enter a unique value",message:err.message})})
 })
 
 
