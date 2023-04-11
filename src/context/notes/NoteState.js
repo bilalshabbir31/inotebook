f63c4519d36c10fdf32c25da48fa77ec2844f862
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-11 22:40:57 +0500

    add context api

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState= (props)=>{
+
+
+    const s1={
+        "name":"harry",
+        "class":"5b"
+    }
+
+    const [state, setState] = useState(s1);
+    
+    const update=()=>{
+        setTimeout(() => {
+            setState({
+                "name":"bill",
+                "class":"10b"
+            })
+        }, 1000);
+    }
+
+    return (
+        <NoteContext.Provider value={{state,update}}>
+            {props.children}
+        </NoteContext.Provider>
+    );
+}
+
+export default NoteState;
