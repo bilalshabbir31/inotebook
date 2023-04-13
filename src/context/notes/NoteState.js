b0aea47dec4a783adf18e880913dfac50d751799
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-13 21:36:35 +0500

    add  notes in home page

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,8 +3,69 @@ import NoteContext from "./noteContext";
 
 const NoteState= (props)=>{
 
+    const notesInitial=[
+
+        {
+        "_id": "6432a87cfec5260fb11d3792",
+        "user": "64329380799a62773dc0c06f",
+        "title": "my title",
+        "description": "please wake up early",
+        "tag": "personal",
+        "date": "2023-04-09T11:58:52.503Z",
+        "__v": 0
+        },
+        {
+        "_id": "6432a886fec5260fb11d3794",
+        "user": "64329380799a62773dc0c06f",
+        "title": "my updated",
+        "description": "please wsam updated up early",
+        "tag": "Youtube",
+        "date": "2023-04-09T11:59:02.751Z",
+        "__v": 0
+        },
+        {
+        "_id": "6432a87cfec5260fb11d3792",
+        "user": "64329380799a62773dc0c06f",
+        "title": "my title",
+        "description": "please wake up early",
+        "tag": "personal",
+        "date": "2023-04-09T11:58:52.503Z",
+        "__v": 0
+        },
+        {
+        "_id": "6432a886fec5260fb11d3794",
+        "user": "64329380799a62773dc0c06f",
+        "title": "my updated",
+        "description": "please wsam updated up early",
+        "tag": "Youtube",
+        "date": "2023-04-09T11:59:02.751Z",
+        "__v": 0
+        },
+        {
+        "_id": "6432a87cfec5260fb11d3792",
+        "user": "64329380799a62773dc0c06f",
+        "title": "my title",
+        "description": "please wake up early",
+        "tag": "personal",
+        "date": "2023-04-09T11:58:52.503Z",
+        "__v": 0
+        },
+        {
+        "_id": "6432a886fec5260fb11d3794",
+        "user": "64329380799a62773dc0c06f",
+        "title": "my updated",
+        "description": "please wsam updated up early",
+        "tag": "Youtube",
+        "date": "2023-04-09T11:59:02.751Z",
+        "__v": 0
+        }
+    ]
+
+    const [notes, setNotes] = useState(notesInitial);
+    
+
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     );
