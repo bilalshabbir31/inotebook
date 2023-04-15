8dd547f7286d96fe5d40e8a830463244bb01f496
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-15 22:37:56 +0500

    add note functionality on client side

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,20 +1,24 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import {React,useContext} from "react";
 import NoteItem from "./NoteItem";
+import AddNote from "./AddNote";
+import noteContext from "../context/notes/noteContext";
 
 function Notes() {
 
     const context = useContext(noteContext);
   
-    const {notes,setNotes}=context;
+    const {notes,addNote}=context;
 
     return (
-        <div className="row my-3">
-            <h1>Your Notes</h1>
-            {notes.map((note)=>{
-            return <NoteItem note={note}/>
-            })}
-        </div>
+        <>
+            <AddNote/>
+            <div className="row my-3">
+                <h1>Your Notes</h1>
+                {notes.map((note)=>{
+                return <NoteItem note={note}/>
+                })}
+            </div>
+        </>
     );
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -63,9 +63,32 @@ const NoteState= (props)=>{
 
     const [notes, setNotes] = useState(notesInitial);
     
+    // add note
+    const addNote=(title,description,tag)=>{
+        console.log("Adding a new note");
+        const note={
+            "_id": "6432a886fec5260fb11d3794aa34",
+            "user": "64329380799a62773dc0c06f",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2023-04-09T11:59:02.751Z",
+            "__v": 0
+            };
+        setNotes(notes.concat(note))
+    }
+    // delete a note
+    const deleteNote=()=>{
+
+    }
+    // edit a note
+    const editNote=()=>{
+
+    }
+
 
     return (
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote}}>
             {props.children}
         </NoteContext.Provider>
     );
