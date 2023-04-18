51a3abf74801a23961ec0946c77e852cc44879be
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-18 22:06:10 +0500

    fix modal issues

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,4 +1,4 @@
-import {React,useContext, useEffect} from "react";
+import { React, useContext, useEffect, useRef } from "react";
 import NoteItem from "./NoteItem";
 import AddNote from "./AddNote";
 import noteContext from "../context/notes/noteContext";
@@ -6,21 +6,49 @@ import noteContext from "../context/notes/noteContext";
 function Notes() {
 
     const context = useContext(noteContext);
-  
-    const {notes,getNotes}=context;
+
+    const { notes, getNotes } = context;
 
     useEffect(() => {
-      getNotes();
-    },[]);
-    
+        getNotes();
+    }, []);
+
+    const ref = useRef("");
+
+    const updateNote = (note) => {
+        
+        ref.current.click();
+    }
 
     return (
         <>
-            <AddNote/>
+            <AddNote />
+            <button ref={ref} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="row my-3">
                 <h1>Your Notes</h1>
-                {notes.map((note)=>{
-                return <NoteItem note={note}/>
+                {notes.map((note) => {
+                    return <NoteItem note={note} updateNote={updateNote} />
                 })}
             </div>
         </>
