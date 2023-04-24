efdfb5ccdd24646517cc373ed7069587ea489a50
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-04-24 06:58:21 +0500

    update signup form

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,16 +10,17 @@ const JWT_SECERT='bilalshabbir';
 // Route 1: Create a user using: POST "api/auth/"
 router.post('/createuser',[body('email').isEmail(),body('name').isLength({ min: 5 }),body('password').isLength({ min: 5 })],
   async (req,res)=>{
+    let success=false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success,errors: errors.array() });
     }
 
     try{
 
       let user = await User.findOne({ email: req.body.email });
       if (user){
-        return res.status(400).json({error: "sorry user already exist"})
+        return res.status(400).json({success,error: "sorry user already exist"})
       }
       const salt=await bcrypt.genSalt(10);
       const secPass=await bcrypt.hash(req.body.password,salt)
@@ -36,7 +37,8 @@ router.post('/createuser',[body('email').isEmail(),body('name').isLength({ min: 
       }
 
       const authtoken= jwt.sign(data,JWT_SECERT);
-      res.json({authtoken});
+      success=true
+      res.json({success,authtoken});
     }catch(error){
       console.log(error.message);
       res.status(500).send("Internal Server error occured!")
